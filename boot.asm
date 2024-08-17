df1cd8b8d99c87fbd7caa0db0d75317e46fad126
elchukc <clareelchuk@gmail.com>
2024-08-17 08:23:31 -0400

    rm hello world message printing

--- boot.asm
+++ boot.asm
@@ -12,20 +12,6 @@ times 33 db 0
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
-
-handle_one:
-    mov ah, 0eh
-    mov al, 'V'
-    mov bx, 0x00
-    int 0x10
-    iret
-
 step2:
     cli ; Clear Interrupts
     mov ax, 0x7c0
@@ -35,17 +21,6 @@ step2:
     mov ss, ax
     mov sp, 0x7c00
     sti ; Enables Interrupts
-
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
-
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
-
-    int 1
-
-    mov si, message
-    call print
 
     jmp $
 
@@ -64,8 +39,6 @@ print_char:
     mov ah, 0eh
     int 0x10
     ret
-
-message: db 'Hello World!', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
