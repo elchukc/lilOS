32e4cbb36ed3ab048c786a66078a61ed8e99fdac
elchukc <clareelchuk@gmail.com>
2024-08-22 11:49:26 -0400

    playing with writing characters to the screen

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,9 @@
 #include "kernel.h"
 
 void kernel_main() {
-    
+    char* video_mem = (char*)(0xB8000);
+    video_mem[0] = 'A';
+    video_mem[1] = 1;
+    video_mem[2] = 'C';
+    video_mem[3] = 3;
 }
