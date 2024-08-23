d1144657b0204be1b8e83863bb3077fc3ddcd597
elchukc <clareelchuk@gmail.com>
2024-08-23 11:31:13 -0400

    WIP interrupt descriptor table

---
+++ src/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Correspond to consts in kernel.asm
+#define KERNEL_CODE_SELECTOR 0x08 // CODE_SEG equ 0x08
+#define KERNEL_DATA_SELECTOR 0x10 // DATA_SEG equ 0x10
+
+
+#define MOLAINOS_TOTAL_INTERRUPTS 512
+
+#endif
