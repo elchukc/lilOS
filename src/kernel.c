51b2a6f69013875fc97f370d8a6f83d2911a25f4
elchukc <clareelchuk@gmail.com>
2024-08-28 14:11:21 -0400

    enable/disable interrupts function

--- src/kernel.c
+++ src/kernel.c
@@ -66,6 +66,9 @@ void kernel_main() {
     // Initialize the interrupt descriptor table
     idt_init();
 
+    // Enable the system interrupts
+    enable_interrupts();
+
     void* ptr = kmalloc(50);
     void* ptr2 = kmalloc(5000);
     void* ptr3 = kmalloc(5600);
