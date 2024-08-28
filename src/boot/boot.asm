1842a28ded5309c764675fae739840e98d1b786a
elchukc <clareelchuk@gmail.com>
2024-08-28 13:36:09 -0400

    start to implement file system

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -3,13 +3,33 @@ BITS 16
 
 CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
-_start:
-    jmp short start
-    nop
 
-; To get around USB emulation.
-; See https://wiki.osdev.org/FAT#BPB_(BIOS_Parameter_Block)
-times 33 db 0
+jmp short start
+nop
+
+; FAT16 Header
+OEMIdentifier       db 'LILOS   '
+BytesPerSector      dw 0x200 ; This is generally ignored by most kernels
+SectorsPerCluster   db 0x80
+ReservedSectors     dw 200
+FATCopies           db 0x02
+RootDirEntries      dw 0x40
+NumSectors          dw 0x00
+MediaType           db 0xF8
+SectorsPerFat       dw 0x100
+SectorsPerTrack     dw 0x20
+NumberOfHeads       dw 0x40
+HiddenSectors       dd 0x00
+SectorsBig          dd 0x773594
+
+; Extended BPB (Dos 4.0)
+DriveNumber         db 0x80
+WinNTBit            db 0x00
+Signature           db 0x29
+VolumeID            dd 0xD105
+VolumeIDString      db 'LILOS BOOT '
+SystemIDString      db 'FAT16   '
+
 
 start:
     jmp 0:step2
