9859fa6e4c6e8385ca9302ffad1063609c136827
elchukc <clareelchuk@gmail.com>
2024-08-28 15:22:28 -0400

    set up memory paging

--- src/kernel.c
+++ src/kernel.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "idt/idt.h"
 #include "memory/heap/kheap.h"
+#include "memory/paging/paging.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -57,6 +58,8 @@ void print(const char* str) {
     }
 }
 
+static struct paging_4gb_chunk* kernel_chunk = 0;
+
 void kernel_main() {
     terminal_initialize();
     print("Hello world!\nWelcome to the kernel.");
@@ -65,6 +68,15 @@ void kernel_main() {
 
     // Initialize the interrupt descriptor table
     idt_init();
+
+    // Set up paging
+    kernel_chunk = paging_new_4gb(PAGING_IS_WRITEABLE | PAGING_IS_PRESENT | PAGING_ACCESS_FROM_ALL);
+
+    // Switch to kernel paging chunk
+    paging_switch(paging_4gb_chunk_get_directory(kernel_chunk));
+
+    // Enable paging
+    enable_paging();
 
     // Enable the system interrupts
     enable_interrupts();
