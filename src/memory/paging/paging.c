89b0eb6f5f14041560df0bb580a18f4706ddcb3c
elchukc <clareelchuk@gmail.com>
2024-08-29 08:48:12 -0400

    enable paging

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -1,5 +1,6 @@
 #include "paging.h"
 #include "memory/heap/kheap.h"
+#include "status.h"
 
 void paging_load_directory(uint32_t* directory);
 
@@ -27,6 +28,45 @@ void paging_switch(uint32_t* directory) {
     current_directory = directory;
 }
 
+bool paging_is_aligned(void* addr) {
+    return ((uint32_t)addr % PAGING_PAGE_SIZE) == 0;
+}
+
 uint32_t* paging_4gb_chunk_get_directory(struct paging_4gb_chunk* chunk) {
     return chunk->directory_entry;
+}
+
+int paging_get_indexes(void* virtual_address, uint32_t* directory_index_out, uint32_t* table_index_out) {
+    int res = 0;
+    if (!paging_is_aligned(virtual_address)) {
+        res = -EINVARG;
+        goto out;
+    }
+
+    *directory_index_out = ((uint32_t)virtual_address / (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE));
+    *table_index_out = ((uint32_t) virtual_address % (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE) / PAGING_PAGE_SIZE);
+out:
+    return res;
+}
+
+int paging_set(uint32_t* directory, void* virt, uint32_t val) {
+    if (!paging_is_aligned(virt)) {
+        return -EINVARG;
+    }
+
+    uint32_t directory_index = 0;
+    uint32_t table_index = 0;
+    int res = paging_get_indexes(virt, &directory_index, &table_index);
+    if (res < 0) {
+        return res;
+    }
+
+    uint32_t entry = directory[directory_index];
+    // extract only the address from the page directory entry. Exclude flags.
+    // since the lower bits aren't used when we're aligned to 4096
+    //      and every page table entry is aligned to 4096
+    uint32_t* table = (uint32_t*)(entry & 0xfffff000);
+    table[table_index] = val;
+
+    return 0;
 }
