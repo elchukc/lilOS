649101da01947f6818506c8cfb7634975e36ac02
elchukc <clareelchuk@gmail.com>
2024-08-29 09:37:52 -0400

    introduce abstractions for disk driver

--- src/disk/disk.c
+++ src/disk/disk.c
@@ -1,5 +1,10 @@
 #include "disk.h"
 #include "io/io.h"
+#include "config.h"
+#include "status.h"
+#include "memory/memory.h"
+
+struct disk disk;
 
 int disk_read_sector(int lba, int total, void* buf) {
     outb(0x1F6, (lba >> 24) | 0xE0);
@@ -25,4 +30,25 @@ int disk_read_sector(int lba, int total, void* buf) {
     }
 
     return 0;
+}
+
+void disk_search_and_init() {
+    memset(&disk, 0, sizeof(disk));
+    disk.type = LILOS_DISK_TYPE_REAL;
+    disk.sector_size = LILOS_SECTOR_SIZE;
+}
+
+struct disk* disk_get(int index) {
+    if (index != 0) {
+        return 0;
+    }
+    return &disk;
+}
+
+int disk_read_block(struct disk* idisk, unsigned int lba, int total, void* buf) {
+    if (idisk != &disk) {
+        return -EIO;
+    }
+
+    return disk_read_sector(lba, total, buf);
 }
