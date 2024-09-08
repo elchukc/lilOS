b98086c4e77e8d394c5bf930743c32dbbee3ea4d
elchukc <clareelchuk@gmail.com>
2024-09-08 14:54:14 -0400

    implement VFS open function

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,12 @@
 #include "string.h"
 
+char tolower(char s1) {
+    if (s1 >= 65 && s1 <= 90) {
+        s1 += 32;
+    }
+    return s1;
+}
+
 int strlen(const char* ptr) {
     int i = 0;
     while(*ptr != 0) {
@@ -18,6 +25,46 @@ int strnlen(const char* ptr, int max) {
     }
 
     return i;
+}
+
+/** Length of str until it finds a null terminator or terminator specified */
+int strnlen_terminator(const char* str, int max, char terminator) {
+    int i = 0;
+    for (i = 0; i < max; i++) {
+        if (str[i] == '\0' || str[i] == terminator)
+            break;
+    }
+
+    return i;
+}
+
+int istrncmp(const char* s1, const char* s2, int n) {
+    unsigned char u1, u2;
+    while (n-- > 0) {
+        u1 = (unsigned char)*s1++;
+        u2 = (unsigned char)*s2++;
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+            return u1 - u2;
+        if (u1 == '\0')
+            return 0;
+    }
+
+    return 0;
+}
+
+int strncmp(const char* s1, const char* s2, int n) {
+    unsigned char u1, u2;
+
+    while (n-- > 0) {
+        u1 = (unsigned char)*s1++;
+        u2 = (unsigned char)*s2++;
+        if (u1 != u2)
+            return u1 - u2;
+        if (u1 == '\0')
+            return 0;
+    }
+
+    return 0;
 }
 
 char* strcpy(char* dest, const char* src) {
