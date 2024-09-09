3ba8e8b345d8274f02d5d8fc303e3f6cc37aaa0e
elchukc <clareelchuk@gmail.com>
2024-09-09 12:56:51 -0400

    fix memory leak in fclose logic

--- src/fs/file.c
+++ src/fs/file.c
@@ -53,6 +53,12 @@ void fs_init() {
     fs_load();
 }
 
+static void file_free_descriptor(struct file_descriptor* desc) {
+    // index-1 because descriptors start at 1
+    file_descriptors[desc->index-1] = 0x00;
+    kfree(desc);
+}
+
 static int file_new_descriptor(struct file_descriptor** desc_out) {
     int res = -ENOMEM;
     for (int i = 0; i < LILOS_MAX_FILE_DESCRIPTORS; i++) {
@@ -183,6 +189,9 @@ int fclose(int fd) {
     }
 
     res = desc->filesystem->close(desc->private);
+    if (res == LILOS_ALL_OK) {
+        file_free_descriptor(desc);
+    }
 out:
     return res;
 }
