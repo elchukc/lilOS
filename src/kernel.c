a1ab26c258eeb1736afe482828c8ab644f07dd41
elchukc <clareelchuk@gmail.com>
2024-09-12 10:00:42 -0400

    fix potential linker alignment bug in kernel too

--- src/kernel.c
+++ src/kernel.c
@@ -88,7 +88,6 @@ struct gdt_structured gdt_structured[LILOS_TOTAL_GDT_SEGMENTS] = {
 
 void kernel_main() {
     terminal_initialize();
-    print("Hello world!\nWelcome to the kernel.");
 
     memset(gdt_real, 0x00, sizeof(gdt_real));
     gdt_structured_to_gdt(gdt_real, gdt_structured, LILOS_TOTAL_GDT_SEGMENTS);
