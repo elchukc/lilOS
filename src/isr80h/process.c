77e896818ea2e5453ca178e5a951050ccb3a2bc8
elchukc <clareelchuk@gmail.com>
2024-09-16 11:29:38 -0400

    load program by typing name in terminal; no program termination

--- src/isr80h/process.c
+++ src/isr80h/process.c
@@ -1,4 +1,5 @@
 #include "process.h"
+#include "string/string.h"
 #include "task/task.h"
 #include "task/process.h"
 #include "status.h"
@@ -12,8 +13,13 @@ void* isr80h_command6_process_load_start(struct interrupt_frame* frame) {
     if (res < 0)
         goto out;
     
+    /** TODO this should be some environment variables ideally */
+    char path[LILOS_MAX_PATH];
+    strcpy(path, "0:/");
+    strcpy(path+3, filename);
+
     struct process* process = 0;
-    res = process_load_switch(filename, &process);
+    res = process_load_switch(path, &process);
     if (res < 0)
         goto out;
     
