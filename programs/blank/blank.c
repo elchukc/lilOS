6053b54f114fa1ce03befa399733828341c82af7
elchukc <clareelchuk@gmail.com>
2024-09-16 12:07:06 -0400

    strtok function + userland string & memory

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,10 +1,16 @@
 #include "lilos.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "string.h"
 
 int main(int argc, char** argv) {
-    printf("My age is %i\n", 98);
+    char words[] = "I walk across the room and look out";
 
+    const char* token = strtok(words, " ");
+    while(token) {
+        printf("%s\n", token);
+        token = strtok(NULL, " ");
+    }
     while(1) {}
     return 0;
 }
